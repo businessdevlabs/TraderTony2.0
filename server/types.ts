a319afdc593e3f25f625fc5ad327e11ec6493a77
sam <sam@metatokenlabs.com>
2025-05-12 01:13:32 -0700

    add test

---
+++ server/types.ts
@@ -0,0 +1,12 @@
+type OHLC = {
+    close: string;
+    end_time: string;
+    high: string;
+    low: string;
+    market_time: string;
+    open: string;
+    start_time: string;
+    total_volume: number;
+    volume: number;
+  };
+  
