649b1498dd180cfa53459311e15404c2fbd7a707
sam <sam@metatokenlabs.com>
2025-05-25 18:00:07 -0700

    update ohlc 1min

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -12,7 +12,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json({ limit: '50mb' })); // Increase JSON body size limit to 50mb
 
 // API endpoints
 app.get('/ohlc/:timeframe', routes.getOHLCData);
